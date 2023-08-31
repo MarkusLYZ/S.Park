918082aa1069bcdc4973f2e0126703aac4de4c7f
MarkusLYZ <markoos203@gmail.com>
2023-08-31 20:07:59 +0800

    Update sendemail.php

--- sendemail.php
+++ sendemail.php
@@ -19,8 +19,8 @@ $mail = new PHPMailer;
 $mail->isSMTP();                      // Set mailer to use SMTP 
 $mail->Host = 'smtp.gmail.com';       // Specify main and backup SMTP servers 
 $mail->SMTPAuth = true;               // Enable SMTP authentication 
-$mail->Username = 'markoos203@gmail.com';   // SMTP username 
-$mail->Password = 'zxclkj515';   // SMTP password 
+$mail->Username = '';   // SMTP username 
+$mail->Password = '';   // SMTP password 
 $mail->SMTPSecure = 'tls';            // Enable TLS encryption, ssl also accepted 
 $mail->Port = 587;                    // TCP port to connect to 
 
